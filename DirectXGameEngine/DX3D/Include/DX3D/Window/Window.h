cac3000300f1ca151319b373b9de0b40e2d049af
KenshinRR <krreblando15@gmail.com>
2026-05-20 15:21:43 +0800

    Initialization
    Initialized Window, DirectX11, and Logger.
    
    Finished up to Episode 2 of PardCode's tutorial on C++  3D Game Tutorial Series

---
+++ DirectXGameEngine/DX3D/Include/DX3D/Window/Window.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <DX3D/Core/Base.h>
+#include <DX3D/Core/Common.h>
+
+namespace dx3d
+{
+	class Window: public Base
+	{
+	public:
+		explicit Window(const WindowDesc& desc);
+		virtual ~Window() override;
+
+	private:
+		void* m_handle{};
+	};
+}
---
+++ DirectXGameEngine/DX3D/Source/DX3D/Game/Game.cpp
@@ -0,0 +1,20 @@
+#include <DX3D/Game/Game.h>
+#include <DX3D/Window/Window.h>
+#include <DX3D/Graphics/GraphicsEngine.h>
+#include <DX3D/Core/Logger.h>
+
+dx3d::Game::Game(const GameDesc& desc) :
+	Base({*std::make_unique<Logger>(desc.logLevel).release()}),
+	m_LoggerPtr(&m_logger)
+{
+	m_graphicsEngine = std::make_unique<GraphicsEngine>(GraphicsEngineDesc{m_logger});
+	m_display = std::make_unique<Window>(WindowDesc{m_logger});
+
+	m_LoggerPtr->log(Logger::LogLevel::Info, "Game initialized!");
+}
+
+
+dx3d::Game::~Game()
+{
+	m_LoggerPtr->log(Logger::LogLevel::Info, "Game deallocation started!");
+}

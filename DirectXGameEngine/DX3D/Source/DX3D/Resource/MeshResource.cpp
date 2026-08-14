280f3bffb9b3b9a01d62830d2b357b91dadf97ef
KenshinRR <krreblando15@gmail.com>
2026-08-14 14:32:40 +0800

    Fixed texturing of imported 3D objects

--- DirectXGameEngine/DX3D/Source/DX3D/Resource/MeshResource.cpp
+++ DirectXGameEngine/DX3D/Source/DX3D/Resource/MeshResource.cpp
@@ -22,6 +22,9 @@ dx3d::MeshResource::MeshResource(const MeshResourceDesc& desc)
     }
 
     std::vector<Vec3> positions;
+    std::vector<Vec2> texcoords;
+    std::vector<Vec3> normals;
+    std::vector<Vertex> vertices;
     std::vector<ui32> indices;
 
     std::string line;
@@ -36,26 +39,55 @@ dx3d::MeshResource::MeshResource(const MeshResourceDesc& desc)
             iss >> pos.x >> pos.y >> pos.z;
             positions.push_back(pos);
         }
+        else if (prefix == "vt") {
+            Vec2 uv;
+            iss >> uv.x >> uv.y;
+            texcoords.push_back(uv);
+        }
+        else if (prefix == "vn") {
+            Vec3 n;
+            iss >> n.x >> n.y >> n.z;
+            normals.push_back(n);
+        }
         else if (prefix == "f") {
             std::string vStr;
             for (int i = 0; i < 3; ++i) {
                 iss >> vStr;
                 if (!vStr.empty()) {
-                    // Split "vertex/texcoord/normal"
+                    // Format: vertexIndex/texcoordIndex/normalIndex
+                    int vIndex = 0, tIndex = 0, nIndex = 0;
+
                     size_t firstSlash = vStr.find('/');
-                    int vIndex = std::stoi(vStr.substr(0, firstSlash));
-                    indices.push_back(static_cast<ui32>(vIndex - 1));
+                    size_t secondSlash = vStr.find('/', firstSlash + 1);
+
+                    if (firstSlash != std::string::npos) {
+                        vIndex = std::stoi(vStr.substr(0, firstSlash));
+                        if (secondSlash != std::string::npos && secondSlash > firstSlash + 1) {
+                            tIndex = std::stoi(vStr.substr(firstSlash + 1, secondSlash - firstSlash - 1));
+                            nIndex = std::stoi(vStr.substr(secondSlash + 1));
+                        }
+                        else {
+                            // case: v/t
+                            tIndex = std::stoi(vStr.substr(firstSlash + 1));
+                        }
+                    }
+                    else {
+                        // case: v only
+                        vIndex = std::stoi(vStr);
+                    }
+
+                    Vertex v{};
+                    v.position = positions[vIndex - 1];
+                    if (tIndex > 0 && tIndex <= (int)texcoords.size())
+                        v.texcoord = texcoords[tIndex - 1];
+                    if (nIndex > 0 && nIndex <= (int)normals.size())
+                        v.normal = normals[nIndex - 1];
+
+                    indices.push_back(static_cast<ui32>(vertices.size()));
+                    vertices.push_back(v);
                 }
             }
         }
-
-    }
-
-    std::vector<Vertex> vertices;
-    for (auto& pos : positions) {
-        Vertex v{};
-        v.position = pos;
-        vertices.push_back(v);
     }
 
     m_mesh = desc.graphicsDevice.createMesh({ vertices, indices });
